fb8fb8953320581759acc1a82503963fc9397925
jerem <jerem@DESKTOP-SG8G3GU>
2020-04-22 21:02:18 -0400

    intial commit

---
+++ src/edu/westga/cs3151/animalgame/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.westga.cs3151.animalgame.main;
+
+/**
+ * @author Jeremiah Liscum
+ *
+ */
+public class Main extends Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
